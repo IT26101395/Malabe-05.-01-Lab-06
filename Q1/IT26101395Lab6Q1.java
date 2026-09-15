bc38545732f0b208d94ab33998ccd78370f0865c
IT26101395 <it26101395@my.sliit.lk>
2026-09-15 22:07:54 +0530

    Add files via upload

---
+++ Q1/IT26101395Lab6Q1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+     
+public class IT26101395Lab6Q1 {
+    
+	public static void main(String args[]) {
+	 
+	 double number , square , squareRoot;
+	 
+	 Scanner input = new Scanner(System.in);
+	 
+	 System.out.print("Enter a number:  " );
+	 number = input.nextDouble();
+	 
+	 if(number <= 0) {
+	   System.out.println("Error: The number must be greater than zero");
+	   
+     }
+	
+	 else{ 
+	 square = number * number;
+	 squareRoot = Math.sqrt(number);
+	 
+	 System.out.println("The square of" + number + "is: " + square);
+     System.out.println("The square root of" + number + " is: " + squareRoot);
+     }
+    }
+  }	
+	   
+	   
+	   
+	
+	
+	   
+	   
